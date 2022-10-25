9a2924f8f5c7bfcc06dec7a1412974885cd82146
Vinicius Lourenço Ferreira <lourencovinicius00@gmail.com>
2022-10-25 07:00:45 -0300

    Melhoria nos códigos

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento && elemento.localName === 'audio') {        
+        elemento.play();        
+    } else {
+        alert('Elemento não encontrado')        
+    }
 }
 
 
@@ -18,5 +24,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
         tocaSom(idAudio);
     }
+
+    tecla.onkeydown = function (evento) {        
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
     
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
